25a8967d90a2e0a289759a190a83870505c5db87
Ebuka <ebuka2264@yahoo.com>
2023-11-12 12:29:11 +0100

    contract: finalized

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,30 @@ use concordium_std::*;
 use core::fmt::Debug;
 
 
-#[derive(Serialize, SchemaType, Debug, PartialEq, Eq)]
-pub struct State{
-    transactions: collections::BTreeMap<u32,Proposal>,
-    admins: Vec<Address>,
-    //transactions: Vec<Proposal>
+#[derive(Debug, Serial, DeserialWithState)]
+#[concordium(state_parameter = "S")]
+pub struct State<S: HasStateApi = StateApi> {
+    transactions: StateMap<u32,Proposal,S>,
+    admins: StateBox<Vec<Address>,S>,
+}
+
+impl State {
+    pub fn voters(&self) -> usize {
+        self.admins.len()
+    }
+    pub fn is_owner(&self, sender: &Address) -> bool {
+        self.admins.contains(sender) 
+     }
 }
 
 pub trait IsOwner {
-    fn is_owner(&self, host: &Host<State>, sender: &Address) -> bool {
+     fn is_owner(&self, host: &Host<State>, sender: &Address) -> bool {
        host.state().admins.contains(sender) 
     }
 
     fn voters(&self, host: &Host<State>) -> usize {
         host.state().admins.len()
     }
-
 }
 
 // proposal <amount, address>
@@ -29,7 +37,7 @@ pub trait IsOwner {
 pub struct Proposal {
     index: u32,
     amount: Amount,
-    receiptient: Address,
+    receiptient: AccountAddress,
     voted: Vec<Address>,
     approvals: u8,
     fufilled: bool,
@@ -41,32 +49,35 @@ impl IsOwner for Proposal{}
 impl Proposal {
 
     pub fn new(
-        index:u32, amount: Amount, receiptient: Address,
+        index:u32, amount: Amount, receiptient: AccountAddress,
         approvals:u8, owner:Address, 
     ) -> Self {
         let voted = Vec::new();
         Proposal{index,amount,receiptient,voted,approvals,fufilled:false,owner}
     }
 
-    pub fn vote(&mut self,ctx: &ReceiveContext, host: &Host<State>)->Result<bool,Error> {
+    pub fn vote(&mut self,ctx: &ReceiveContext, votes_needed: usize )->Result<bool,Error> {
        if self.voted.contains(&ctx.sender()) {
             return Err(Error::AlreadyVoted)
        }else {
-            let sender = ctx.sender().clone();
-            self.voted.push(sender);
+            self.voted.push(ctx.sender());
             self.approvals += 1;
-            let num_of_voted = self.voters(host) as u8;
-            Ok(self.approvals == num_of_voted)
+            Ok(self.approvals == votes_needed as u8)
        }
+    }
+    pub fn approved(&self, votes_needed: usize ) -> Result<bool,Error>{
+        let num_of_voted = votes_needed as u8;
+        Ok(self.approvals == num_of_voted)
     }
     
 }
 
 impl State {    
-    pub fn new()-> Self {
-        let transactions = collections::BTreeMap::new();
-        let admins = Vec::new();
-        State { transactions, admins }
+    pub fn new(state_builder: &mut StateBuilder, admins: Vec<Address> )-> Self {
+            State { 
+                transactions: state_builder.new_map(), 
+                admins: state_builder.new_box(admins) 
+            }
     }
 }
 
@@ -78,7 +89,8 @@ pub enum Error {
     ParseParams,
     /// Your error
     YourError,
-    AlreadyVoted
+    AlreadyVoted,
+    TransactionHasNotBeenApprovedOrAlreadyFufilled
 }
 #[derive(Serialize, SchemaType, Debug, PartialEq, Eq)]
 pub struct InitParameter {
@@ -87,8 +99,13 @@ pub struct InitParameter {
 #[derive(Serialize, SchemaType)]
 pub struct TxParameter {
     index: u32,
-    receiver : Address,
+    receiver : AccountAddress,
     amount: Amount,
+}
+
+#[derive(Serialize, SchemaType, Debug, PartialEq, Eq)]
+pub struct ApproveParameter {
+    index: u32,
 }
 
 /// Init function that creates a new smart contract.
@@ -96,32 +113,41 @@ pub struct TxParameter {
     contract = "ccd_multisig",
     parameter="InitParameter"
 )]
-fn init(ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State> {
+fn init(ctx: &InitContext, state_builder: &mut StateBuilder) -> InitResult<State> {
     // Your code
     let param: InitParameter = ctx.parameter_cursor().get()?;
-    let mut state = State::new();
     let admins = param.admins;
-    admins.iter().for_each(|i| state.admins.push(*i));
+    let state = State::new(state_builder,admins);
     Ok(state)
+   
 }
 
 
 #[receive( contract = "ccd_multisig", name = "transfer", parameter="TxParameter", mutable)]
-fn transfer(ctx: &ReceiveContext, host: &Host<State>)-> ReceiveResult<()> {
+fn transfer(ctx: &ReceiveContext, host: &mut Host<State>)-> ReceiveResult<()> {
     let param:TxParameter = ctx.parameter_cursor().get()?;
+    let votes_needed = host.state().admins.len();
     let index = param.index;
-    let state = *host.state_mut();
-    let proposal = host.state_mut().transactions.get_mut(&index).unwrap();
-
-    let result = host.invoke_transfer(&proposal.receiptient, proposal.amount);
-
-    match result{
-        Ok(()) => {
-            proposal.fufilled = true;
-        }
-        Err(_) => {bail!()}
+    let approved = host.state_mut()
+        .transactions
+        .get(&index).unwrap().approved(votes_needed).unwrap();
+    let not_fufilled = host.state_mut()
+        .transactions
+        .get_mut(&index).unwrap().fufilled == false;
+    match (approved,not_fufilled) {
+        (true,true) => {
+            host.state_mut()
+            .transactions
+            .get_mut(&index).unwrap().fufilled = true;
+            let response = host.invoke_transfer(&param.receiver, param.amount);
+            match response{
+                Ok(()) => Ok(()),
+                Err(_) => bail!()
+            }
+        },
+        _ => bail!()
     }
-    Ok(())
+    
 }
 
 /// This function recieves CCD from anybody
@@ -131,14 +157,36 @@ fn insert(ctx: &ReceiveContext,host: &Host<State>,amount: Amount
 ) -> ReceiveResult<()> {
     Ok(())
 }
-#[receive(contract = "ccd_multisig", name = "create_tx", mutable)]
-pub fn create_tx(_ctx: &ReceiveContext,host: &mut Host<State>)-> ReceiveResult<u32>{
+
+/// initialises a new transaction pending approval
+#[receive(contract = "ccd_multisig", name = "create_tx", parameter="TxParameter", mutable)]
+pub fn create_tx(ctx: &ReceiveContext,host: &mut Host<State>)-> ReceiveResult<u32>{
     let index = host.state().admins.len() as u32;
+    let param:TxParameter = ctx.parameter_cursor().get()?;
+    let proposal = Proposal::new(index,param.amount,param.receiver,0,ctx.sender());
+    host.state_mut().transactions.insert(index, proposal);
     Ok(index)
 }
 
-/// View function that returns the content of the state.
-#[receive(contract = "ccd_multisig", name = "view", return_value = "State")]
-fn view<'b>(_ctx: &ReceiveContext, host: &'b Host<State>) -> ReceiveResult<&'b State> {
-    Ok(host.state())
+#[receive(contract = "ccd_multisig", name = "approve", parameter="ApproveParameter", mutable)]
+pub fn approve(ctx: &ReceiveContext,host: &mut Host<State>)-> ReceiveResult<bool>{
+    let param:ApproveParameter = ctx.parameter_cursor().get()?;
+    let index = param.index;
+    let votes_needed = host.state().voters();
+    let mut proposal = host.state_mut().transactions.get_mut(&index).unwrap();
+    ensure_eq!(index,proposal.index);
+    let approved = proposal.vote(ctx,votes_needed)?;
+    Ok(approved)
+}
+
+///View function that returns the content of the state.
+#[receive(contract = "ccd_multisig", name = "view", return_value = "Proposal")]
+fn view<'a, 'b>(ctx: &'a ReceiveContext, host: &'b Host<State>) ->   ReceiveResult<Proposal> {
+    let param:ApproveParameter = ctx.parameter_cursor().get()?;
+    let prop = host.state().transactions.get(&param.index).unwrap();
+    let mut voted = Vec::new();
+    prop.voted.iter().for_each(|i| voted.push(*i));
+    let (index,amount,receiptient, approvals,fufilled, owner) = (prop.index,prop.amount,prop.receiptient,prop.approvals,prop.fufilled,prop.owner);
+    Ok(Proposal{index,amount,receiptient,voted,approvals,fufilled,owner})
+
 }
